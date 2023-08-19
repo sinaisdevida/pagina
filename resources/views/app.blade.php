08cb676ab9c1e8c025092d86f99df93d35c2f19c
leandro <leandroj.r.ximenes@gmail.com>
2023-08-19 20:38:42 +0000

    first page

---
+++ resources/views/app.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sinais de vida</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="flex flex-col justify-center my-2">
+        <div class="w-full flex justify-center">
+            <img src="{{ asset('img/site.png') }}" alt="Logo">
+        </div>
+        <h1 class="text-3xl font-bold underline text-center mt-4">
+            Site em construção
+        </h1>
+    </div>
+</body>
+
+</html>
